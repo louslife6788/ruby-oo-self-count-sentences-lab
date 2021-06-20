48da7466a63845b7640c26c7b5199d623a57ec57
luis leon <leonluis380@yahoo.com>
2021-06-20 11:46:28 -0400

    commit tree 0a9cae4c3da5bd77c55e2522c9637d72cd35af23

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+      self.end_with?(".")
+    #   return true
+    #  else 
+    #   return false
+    #  end // this is the same as the above 
   end
 
   def question?
+    self.end_with?('?')
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
 
   end
 end
